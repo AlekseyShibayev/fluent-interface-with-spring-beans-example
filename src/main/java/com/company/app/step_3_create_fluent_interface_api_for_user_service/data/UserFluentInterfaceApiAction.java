f7ebfc5ce4ce69ada5114dc81a4bcb0a973cc1ab
Шибаев Алексей Евгеньевич (4094442) <VTB4094442@region.vtb.ru>
2023-08-28 20:51:28 +0300

    step 3 rdy

--- src/main/java/com/company/app/step_3_create_fluent_interface_api_for_user_service/data/UserFluentInterfaceApiAction.java
+++
@@ -1,8 +0,0 @@
-package com.company.app.step_3_create_fluent_interface_api_for_user_service.data;
-
-@FunctionalInterface
-public interface UserFluentInterfaceApiAction {
-
-    void doAction(UserFluentInterfaceApiContext context);
-
-}
